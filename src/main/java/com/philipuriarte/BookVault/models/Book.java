80a92fd4b8ca0e04dd3a05b8ca10afefd850d03f
Philip Uriarte <philipuriarte@gmail.com>
2025-01-07 13:25:01 +0800

    Create books table

---
+++ src/main/java/com/philipuriarte/BookVault/models/Book.java
@@ -0,0 +1,86 @@
+package com.philipuriarte.BookVault.models;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String author;
+    private String publisher;
+    private String genre;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Date dateAdded;
+    private String imageFileName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getImageFileName() {
+        return imageFileName;
+    }
+
+    public void setImageFileName(String imageFileName) {
+        this.imageFileName = imageFileName;
+    }
+}
