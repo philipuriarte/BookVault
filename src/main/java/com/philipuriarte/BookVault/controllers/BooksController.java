31a5b4c770abf5a4985ef37d9b6dfdf7cf198b4e
Philip Ronin Uriarte <philipuriarte@gmail.com>
2025-01-09 23:41:14 +0800

    Setup Create functionality

--- src/main/java/com/philipuriarte/BookVault/controllers/BooksController.java
+++ src/main/java/com/philipuriarte/BookVault/controllers/BooksController.java
@@ -2,6 +2,7 @@ package com.philipuriarte.BookVault.controllers;
 
 import com.philipuriarte.BookVault.models.Book;
 import com.philipuriarte.BookVault.models.BookDto;
+import jakarta.validation.Path;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,7 +14,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.philipuriarte.BookVault.services.BooksRepository;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.*;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -48,6 +54,36 @@ public class BooksController {
         if (result.hasErrors()) {
             return "books/addbook";
         }
+
+        // Save image file
+        MultipartFile image = bookDto.getImageFile();
+        String storageFileName = image.getOriginalFilename();
+
+        try {
+            String uploadDir = "public/images/";
+            java.nio.file.Path uploadPath = java.nio.file.Paths.get(uploadDir);
+
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()){
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+
+        Book book = new Book();
+        Date dateAdded = new Date();
+        book.setTitle(bookDto.getTitle());
+        book.setAuthor(bookDto.getAuthor());
+        book.setPublisher(bookDto.getPublisher());
+        book.setGenre(bookDto.getGenre());
+        book.setDateAdded(dateAdded);
+        book.setImageFileName(storageFileName);
+
+        repo.save(book);
 
         return "redirect:/books/home";
     }
