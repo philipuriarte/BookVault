be1f48b208abce6c67c3f96db5bea794e50e7eda
Philip Ronin Uriarte <philipuriarte@gmail.com>
2025-01-09 22:01:32 +0800

    Finish addbook page

--- src/main/java/com/philipuriarte/BookVault/controllers/BooksController.java
+++ src/main/java/com/philipuriarte/BookVault/controllers/BooksController.java
@@ -24,7 +24,7 @@ public class BooksController {
         return "books/home";
     }
 
-    @GetMapping("/add")
+    @GetMapping("/addbook")
     public String showAddPage(Model model) {
         BookDto bookDto = new BookDto();
         model.addAttribute("bookDto", bookDto);
