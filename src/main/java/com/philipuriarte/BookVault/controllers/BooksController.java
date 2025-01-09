16f0a65474c83b23170bd017c302e645b4701ac9
Philip Ronin Uriarte <philipuriarte@gmail.com>
2025-01-09 23:06:27 +0800

    Add input validation in addbook

--- src/main/java/com/philipuriarte/BookVault/controllers/BooksController.java
+++ src/main/java/com/philipuriarte/BookVault/controllers/BooksController.java
@@ -2,10 +2,15 @@ package com.philipuriarte.BookVault.controllers;
 
 import com.philipuriarte.BookVault.models.Book;
 import com.philipuriarte.BookVault.models.BookDto;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.philipuriarte.BookVault.services.BooksRepository;
 
@@ -29,5 +34,21 @@ public class BooksController {
         BookDto bookDto = new BookDto();
         model.addAttribute("bookDto", bookDto);
         return "books/addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(
+            @Valid @ModelAttribute BookDto bookDto,
+            BindingResult result
+            ) {
+        if (bookDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("bookDto", "imageFile", "The image file is required."));
+        }
+
+        if (result.hasErrors()) {
+            return "books/addbook";
+        }
+
+        return "redirect:/books/home";
     }
 }
